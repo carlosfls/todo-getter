02673e3396aaa85bab26270f30a9928fe1feddea
Carlos <carlosfls54@gmail.com>
2026-06-15 17:53:10 -0300

    Agregando JsonIgnore para propiedades que no queremos mapear

--- src/main/java/org/carlosacademic/model/TodoDTO.java
+++ src/main/java/org/carlosacademic/model/TodoDTO.java
@@ -1,4 +1,7 @@
 package org.carlosacademic.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record TodoDTO(int userId, int id, String title, boolean completed) {
 }
--- src/main/java/org/carlosacademic/model/UserDTO.java
+++ src/main/java/org/carlosacademic/model/UserDTO.java
@@ -1,4 +1,7 @@
 package org.carlosacademic.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record UserDTO(int id, String name, String username, String email) {
 }
